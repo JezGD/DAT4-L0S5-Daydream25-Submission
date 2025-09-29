fcfc126987386048bb6500e79d8b08bf8fe89157
Potatobruh <203572682+JezGD@users.noreply.github.com>
2025-09-28 18:29:37 -0600

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 50.0
+const JUMP_VELOCITY = 0
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("U") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("l", "r")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.play("IDLE")
+		animated_sprite.flip_h = direction < 0 
+		animated_sprite.play("IDLE")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
